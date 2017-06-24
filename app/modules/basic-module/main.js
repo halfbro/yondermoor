4c8ee80772e01483b6c8a648c6865663478e13bb
Meshiest <meek.mesh@gmail.com>
2017-06-23 22:37:07 -0500

    Better island generation

--- app/modules/basic-module/main.js
+++ app/modules/basic-module/main.js
@@ -43,17 +43,27 @@ module.exports = class extends Module.Generator {
     let noise = perlin.generatePerlinNoise(width, width, {
       octaveCount: 4,
       amplitude: 0.1,
-      persistence: 0.5,
+      persistence: 0.3,
     });
 
     let min = Math.min(...noise), max = Math.max(...noise);
 
     for(let x = 0; x < width; x++) {
       for(let y = 0; y < width; y++) {
-        let depth = (noise[x * width + y] - min) / (max - min) * config.HEIGHT - config.WATERLEVEL;
+        let depth = (noise[x * width + y]);
         let dist = Math.min(Math.hypot(x - width/2, y - width/2) / width * 1.8, 1);
         //depth *= Math.sqrt(1 - dist);
-        depth *= Math.pow(Math.cos(dist * Math.PI ) * 0.5 + 0.5, 2);
+        depth *= Math.pow(Math.cos(dist * Math.PI ) * 0.5 + 0.5, 1);
+        depth -= config.WATERLEVEL / config.HEIGHT;
+        if(depth > 0) {
+          if(depth > (max - min) * 0.2)
+            depth = 1;
+          else
+            depth = 0.5;
+        }
+        depth *= config.HEIGHT;
+
+        //depth = Math.round(depth * 2 + 0.45) / 2;
         createBox(x - width/2, y - width/2, depth);
       }
     }
